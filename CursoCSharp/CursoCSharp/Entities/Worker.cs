e710ce80f3220c222c83aa3f953288e6fc4ee0db
Gabriel Ribeiro <gabrielderibeiroo@gmail.com>
2022-11-07 18:11:18 -0300

    Criada as classes do projeto de estudo

--- CursoCSharp/CursoCSharp/Entities/Worker.cs
+++ CursoCSharp/CursoCSharp/Entities/Worker.cs
@@ -9,6 +9,43 @@ namespace CursoCSharp.Entities
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
 
-        // agora comeca a composicao de objetos
+        // composicao de objetos
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+
+            }
+            return sum;
+        }
     }
 }
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using CursoCSharp.Entities.Enum;
+using CursoCSharp.Entities;
 
 namespace CursoCSharp
 {
@@ -6,7 +9,38 @@ namespace CursoCSharp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter departament's name: ");
+            string deptName = Console.ReadLine();
+
+            //Console.Write("Enter worker date: ");
+
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter level (Junior, Midlevel or Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+            Console.Write("Enter base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Departament dept = new Departament(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("Worker many contracts to this worker?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n ; i++)
+            {
+                Console.WriteLine($"Enter #{1} contract date: ");
+
+                Console.WriteLine("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+            }
+
+
+
+
         }
     }
 }
+
